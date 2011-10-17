a93c3357f50e91cde6df9449b5ea664792de75f1
Eduardo R. D'Avila <erdavila@gmail.com>
2011-10-17 17:06:21 -0200

    Generalizing template addition<>.

--- bits/operations.hpp
+++ bits/operations.hpp
@@ -7,24 +7,28 @@
 
 
 
-template <typename SIValue1, typename SIValue2>
+template <typename T1, typename T2>
 struct addition {
+	typedef decltype(*static_cast<T1*>(0) + *static_cast<T2*>(0)) type;
+};
+
+
+template <typename ValueType1, typename Ratio1, int... BaseUnitPowers1,
+          typename ValueType2, typename Ratio2, int... BaseUnitPowers2>
+struct addition<Value<ValueType1, Ratio1, BaseUnitPowers1...>,
+                Value<ValueType2, Ratio2, BaseUnitPowers2...>>
+{
 private:
-	static_assert(std::is_same<typename SIValue1::BaseUnitPowersList,
-	                           typename SIValue2::BaseUnitPowersList>::value,
+	static_assert(std::is_same<int_list<BaseUnitPowers1...>,
+	                           int_list<BaseUnitPowers2...>>::value,
 	              "The powers of base units must be the same on the addition");
 
-	typedef typename SIValue1::ValueType _ValueType1;
-	typedef typename SIValue2::ValueType _ValueType2;
-	typedef decltype(*static_cast<_ValueType1*>(0) + *static_cast<_ValueType2*>(0)) _NewValueType;
-
-	typedef typename SIValue1::Ratio _Ratio1;
-	typedef typename SIValue2::Ratio _Ratio2;
+	typedef typename addition<ValueType1, ValueType2>::type _NewValueType;
 	static const unsigned int _new_num = 1;
-	static const unsigned int _new_den = std::ratio_add<_Ratio1, _Ratio2>::type::den;
+	static const unsigned int _new_den = std::ratio_add<Ratio1, Ratio2>::type::den;
 	typedef std::ratio<_new_num, _new_den> _NewRatio;
 
-	typedef typename SIValue1::BaseUnitPowersList _NewBaseUnitPowersList;
+	typedef int_list<BaseUnitPowers1...> _NewBaseUnitPowersList;
 
 public:
 	typedef typename make_value<_NewValueType, _NewRatio, _NewBaseUnitPowersList>::type type;
@@ -41,7 +45,7 @@ struct multiplication {
 template <typename ValueType1, typename Ratio1, int... BaseUnitPowers1,
           typename ValueType2, typename Ratio2, int... BaseUnitPowers2>
 struct multiplication<Value<ValueType1, Ratio1, BaseUnitPowers1...>,
-            Value<ValueType2, Ratio2, BaseUnitPowers2...>>
+                      Value<ValueType2, Ratio2, BaseUnitPowers2...>>
 {
 private:
 	typedef typename multiplication<ValueType1, ValueType2>::type _NewValueType;
