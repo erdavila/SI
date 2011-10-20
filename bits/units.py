a5d6119c0cfa05eea948c32ef7e6ebb03a715794
Eduardo R. D'Avila <erdavila@gmail.com>
2011-10-20 18:04:19 -0200

    Additional units.

--- bits/units.py
+++ bits/units.py
@@ -26,8 +26,8 @@ def generate_defs():
 			print_line('//@{')
 			
 			for unit in group.units:
-				print_line('/// %s' % unit.measure)
-				print_line('#define %s(VALUETYPE)\t%s' % (unit.macro(), unit.definition))
+				print_line('/// %s in %s' % (group.name(), unit.plural))
+				print_line('#define %s\t%s' % (unit.macro('VALUETYPE'), unit.definition))
 			
 			print_line('//@}')
 		
@@ -46,13 +46,13 @@ def generate_units_cpp():
 		print_line('')
 		print_line('namespace si {')
 		print_line('namespace units {')
+		print_line('')
 		
 		for group in Group.GROUPS:
 			print_line('')
-			print_line('')
 			
 			for unit in group.units:
-				print_line('const %s(int)\t%s(1);' % (unit.macro(), unit.clean_symbol()))
+				print_line('const %s\t%s(1);' % (unit.macro('int'), unit.clean_symbol()))
 		
 		print_line('')
 		print_line('')
@@ -83,8 +83,8 @@ def generate_units_hpp():
 			print_line('//@{')
 			
 			for unit in group.units:
-				print_line('/// 1%s (1%s)' % (unit.symbol, unit.singular))
-				print_line('extern const %s(int)\t%s;' % (unit.macro(), unit.clean_symbol()))
+				print_line('/// 1%s (1 %s)' % (unit.symbol, unit.singular))
+				print_line('extern const %s\t%s;' % (unit.macro('int'), unit.clean_symbol()))
 			
 			print_line('//@}')
 		
@@ -105,27 +105,28 @@ def main():
 
 
 class Group(object):
-	def __init__(self, is_base_unit, quantity, unit_name, symbol, *units):
+	def __init__(self, is_base_unit, quantity, symbol, *units):
 		self.is_base_unit = is_base_unit
 		self.quantity = quantity
-		self.unit_name = unit_name
 		self.symbol = symbol
 		self.units = units
 		for unit in units:
 			unit.group = self
 		Group.GROUPS.append(self)
 	
-	def group_name(self):
+	def name(self):
 		if isinstance(self.quantity, str):
-			group_name = self.quantity
+			name = self.quantity
 		else:
-			group_name = ', '.join(self.quantity)
-		return group_name.capitalize()
+			name = ', '.join(self.quantity)
+		return name.capitalize()
 	
 	def header(self):
+		unit_name = Unit.UNITS[self.symbol].singular
+		
 		return '\n'.join([
-				'/** @name %s' % self.group_name(),
-				' * %s unit: %s<br>' % ('Base' if self.is_base_unit else 'Derived', self.unit_name),
+				'/** @name %s' % self.name(),
+				' * %s unit: %s<br>' % ('Base' if self.is_base_unit else 'Derived', unit_name),
 				' * Symbol: %s' % self.symbol,
 				' */'
 			])
@@ -134,25 +135,25 @@ class Group(object):
 
 
 class Unit(object):
-	def __init__(self, symbol, measure, singular, definition):
+	def __init__(self, symbol, singular, plural, definition):
 		self.symbol = symbol
-		self.measure = measure
 		self.singular = singular
+		self.plural = plural
 		self.definition = definition
 		if symbol in Unit.UNITS:
 			raise Exception(symbol)
 		Unit.UNITS[symbol] = self
 	
-	def macro(self):
+	def macro(self, argument):
 		quantity = self.group.quantity
 		if not isinstance(quantity, str):
 			quantity = quantity[0]
-		quantity = quantity.upper()
+		quantity = quantity.upper().replace(' ', '')
 		
-		return 'SI_%s_%s' % (quantity, self.clean_symbol())
+		return 'SI_%s_%s(%s)' % (quantity, self.clean_symbol(), argument)
 	
 	def clean_symbol(self):
-		return self.symbol.replace('μ', 'u').replace('/', '_').replace('²', '2')
+		return self.symbol.replace('μ', 'u').replace('/', '_').replace('²', '2').replace('³', '3')
 	
 	UNITS = {}
 
@@ -171,45 +172,82 @@ class ApplyRatio(object):
 		self.ratio = ratio
 	
 	def __str__(self):
-		return '%s(VALUETYPE)::apply_ratio<%s>::type' % (Unit.UNITS[self.from_symbol].macro(), self.ratio)
+		ratio = self.ratio
+		if isinstance(ratio, int):
+			ratio = ' ::std::ratio<%d>' % ratio
+		
+		return '%s::apply_ratio<%s>::type' % (Unit.UNITS[self.from_symbol].macro('VALUETYPE'), ratio)
 
 
-class Division(object):
+class TypeOperation(object):
 	def __init__(self, symbol1, symbol2):
 		self.symbol1 = symbol1
 		self.symbol2 = symbol2
-	
+
 	def __str__(self):
-		macro1 = Unit.UNITS[self.symbol1].macro()
-		macro2 = Unit.UNITS[self.symbol2].macro()
-		return '::si::division<%s(VALUETYPE), %s(VALUETYPE)>::type' % (macro1, macro2)
+		macro1 = 'VALUETYPE' if self.symbol1 is None else Unit.UNITS[self.symbol1].macro('VALUETYPE')
+		macro2 = 'VALUETYPE' if self.symbol2 is None else Unit.UNITS[self.symbol2].macro('VALUETYPE')
+		return '::si::%s<%s, %s>::type' % (self.operation(), macro1, macro2)
+
+
+class Multiplication(TypeOperation):
+	def operation(self):
+		return 'multiplication'
+
+
+class Division(TypeOperation):
+	def operation(self):
+		return 'division'
+	
 
 
 ##### UNITS ####################################################################
 
-Group(True, ['length', 'distance'], 'meter', 'm',
-	Unit('mm', 'Length in millimeters', 'millimeter', ApplyRatio('m', ' ::std::milli')),
-	Unit('cm', 'Length in centimeters', 'centimeter', ApplyRatio('m', ' ::std::centi')),
-	Unit('m' , 'Length in meters'     ,      'meter', SIValue(1)),
-	Unit('km', 'Length in kilometers' ,  'kilometer', ApplyRatio('m', ' ::std::kilo')),
+Group(True, ['length', 'distance'], 'm',
+	Unit('mm', 'millimeter', 'millimeters', ApplyRatio('m', ' ::std::milli')),
+	Unit('cm', 'centimeter', 'centimeters', ApplyRatio('m', ' ::std::centi')),
+	Unit('m' ,      'meter',      'meters', SIValue(1)),
+	Unit('km',  'kilometer',  'kilometers', ApplyRatio('m', ' ::std::kilo')),
 )
 
-Group(True, 'time', 'second', 's',
-	Unit('ns' , 'Time in nanoseconds' ,  'nanosecond', ApplyRatio('s', ' ::std::nano')),
-	Unit('μs' , 'Time in microseconds', 'microsecond', ApplyRatio('s', ' ::std::micro')),
-	Unit('ms' , 'Time in milliseconds', 'millisecond', ApplyRatio('s', ' ::std::milli')),
-	Unit('s'  , 'Time in seconds'     ,      'second', SIValue(0, 0, 1)),
-	Unit('min', 'Time in minutes'     ,      'minute', ApplyRatio('s'  , ' ::std::ratio<%d>' % 60)),
-	Unit('h'  , 'Time in hours'       ,        'hour', ApplyRatio('min', ' ::std::ratio<%d>' % 60)),
-	Unit('d'  , 'Time in days'        ,         'day', ApplyRatio('h'  , ' ::std::ratio<%d>' % 24)),
+Group(True, 'time', 's',
+	Unit('ns' ,  'nanosecond',  'nanoseconds', ApplyRatio('s', ' ::std::nano')),
+	Unit('μs' , 'microsecond', 'microseconds', ApplyRatio('s', ' ::std::micro')),
+	Unit('ms' , 'millisecond', 'milliseconds', ApplyRatio('s', ' ::std::milli')),
+	Unit('s'  ,      'second',      'seconds', SIValue(0, 0, 1)),
+	Unit('min',      'minute',      'minutes', ApplyRatio('s'  , 60)),
+	Unit('h'  ,        'hour',        'hours', ApplyRatio('min', 60)),
+	Unit('d'  ,         'day',         'days', ApplyRatio('h'  , 24)),
 )
 
-Group(False, 'speed', 'meters per second', 'm/s',
-	Unit('m/s', 'Speed in meters per second', 'meter per second', Division('m', 's'))
+Group(True, 'electric current', 'A',
+	Unit('A', 'ampere', 'amperes', SIValue(0, 0, 0, 1)),
 )
 
-Group(False, 'acceleration', 'meters per second squared', 'm/s²',
-	Unit('m/s²', 'Acceleration in meters per second squared', 'meter per second squared', Division('m/s', 's'))
+Group(False, 'area', 'm²',
+	Unit('cm²', 'square centimeter', 'square centimeters', Multiplication('cm', 'cm')),
+	Unit( 'm²', 'square meter'     , 'square meters'     , Multiplication( 'm',  'm')),
+	Unit('km²', 'square kilometer' , 'square kilometers' , Multiplication('km', 'km')),
+)
+
+Group(False, 'volume', 'm³',
+	Unit('m³', 'cubic meter', 'cubic meters', Multiplication('m²', 'm')),
+)
+
+Group(False, 'speed', 'm/s',
+	Unit('m/s', 'meter per second', 'meters per second', Division('m', 's'))
+)
+
+Group(False, 'acceleration', 'm/s²',
+	Unit('m/s²', 'meter per second squared', 'meters per second squared', Division('m/s', 's'))
+)
+
+Group(False, 'electric charge', 'C',
+	Unit('C', 'coulomb', 'coulombs', Multiplication('A', 's'))
+)
+
+Group(False, 'frequency', 'Hz',
+	Unit('Hz', 'hertz', 'hertz', Division(None, 's'))
 )
 
 
